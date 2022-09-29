3d3c8ab7a5d33e837e75febea1f3771a196eee56
Jules Kreuer <contact@juleskreuer.eu>
2022-09-29 16:34:32 +0200

    switch to yaml file for conda repos

--- data/README.md
+++ data/README.md
@@ -5,6 +5,6 @@ Files just above the size-limit were compressed split into multiple files using 
 | Filename | Description |
 | -------- | ----------  |
 | contigs  | 7z archive of the contigs at each step |
-| cat-base-env.conda | Export of the cat-base-env conda enviroment |
-| cat-diamond-env.conda | Export of the cat-diamond-env conda enviroment |
-| cat-megahit-env.conda | Export of the cat-megahit-env conda enviroment |
+| cat-base-env.yaml | Export of the cat-base-env conda enviroment |
+| cat-diamond-env.yaml | Export of the cat-diamond-env conda enviroment |
+| cat-megahit-env.yaml | Export of the cat-megahit-env conda enviroment |
--- pipeline/init.sh
+++ pipeline/init.sh
@@ -33,31 +33,13 @@ mkdir -p $WS/data/trimmed
 source $CONDA_SH_PATH
 
 conda env remove -n cat-base-env
-conda env remove -n cat-megahit-env
 conda env remove -n cat-diamond-env
+conda env remove -n cat-megahit-env
 
-conda create -n cat-base-env python=3.10 -y
+conda env create --file $WS/data/cat-base-env.yaml
+conda env create --file $WS/data/cat-diamond-env.yaml
+conda env create --file $WS/data/cat-megahit-env.yaml
 
-conda activate cat-base-env 
-conda install -c bioconda sra-tools=2.11.0 -y
-conda install -c bioconda bwa=0.7.17 -y
-conda install -c bioconda samtools=1.6 -y
-conda install -c bioconda trimmomatic=0.39 -y
-conda install -c conda-forge minifasta=2.4 -y
-conda install -c bioconda fastq=1.2.2 -y
-conda install -c bioconda fastqc -y
-conda install unzip -y
-
-conda create -n cat-megahit-env python=3.6 -y
-
-conda activate cat-megahit-env
-conda install -c bioconda megahit=1.1.2 -y
-
-conda create -n cat-diamond-env python=3.10 -y
-
-conda activate cat-diamond-env
-conda install -c bioconda diamond=2.0.15 -y
-conda install -c bioconda megan=6.21.7 -y
 
 # Install version 0.38 of cd-hit
 # Or download the binary and adapt the path in "pipeline/filter_cdhit_diamond_megan.sh"
